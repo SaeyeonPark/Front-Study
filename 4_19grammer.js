0f213511e83b721f39f94509e3beb68d813499ac
parksaeyeon <saeyonpark4@gmail.com>
2022-07-01 19:18:11 +0900

    study js

---
+++ 4_19grammer.js
@@ -0,0 +1,9 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+//let myVariable;
+myVariable = 'Bob';
+
+let myVariable = 'Bob';
+myVariable;
+myVariable = 'Steve';
